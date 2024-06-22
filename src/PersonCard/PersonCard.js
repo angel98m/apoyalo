22cb7bf94ee202d47d271c4e78dcc3df525d443c
angel98m <angel98morinigo@gmail.com>
2024-06-22 06:43:12 -0400

    Poniendo todo junto

--- src/PersonCard/PersonCard.js
+++ src/PersonCard/PersonCard.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
+  const [age, setAge] = useState(props.age);
+
+  const handleBirthday = () => {
+    setAge(age + 1);
+  };
+
   return (
     <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
       <h2>{props.lastName}, {props.firstName}</h2>
-      <p>Age: {props.age}</p>
+      <p>Age: {age}</p>
       <p>Hair-Color: {props.hairColor}</p>
+      <button onClick={handleBirthday}>Birthday Button for {props.firstName}</button>
     </div>
   );
 }
